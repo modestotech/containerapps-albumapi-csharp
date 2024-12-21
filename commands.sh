41bc644b1b00ea881a3ff91821735711cc0681e0
Max Wallin <x-mawa@oresundsbron.com>
2024-12-21 21:35:51 +0100

    Added sh file

---
+++ commands.sh
@@ -0,0 +1,25 @@
+BASE="album-containerapps"
+RESOURCE_GROUP="rg-"$BASE
+IDENTITY="mi-"$BASE
+LOCATION="westeurope"
+ENVIRONMENT="env-"$BASE
+API_NAME="album-api"
+FRONTEND_NAME="album-ui"
+GITHUB_USERNAME="modestotech"
+ACR_NAME="acaalbums"$GITHUB_USERNAME
+
+
+
+az group create --name $RESOURCE_GROUP --location "$LOCATION"
+
+az acr create --resource-group $RESOURCE_GROUP --location $LOCATION --name $ACR_NAME --sku Basic
+
+az acr config authentication-as-arm show --registry "$ACR_NAME"
+
+az identity create --name $IDENTITY --resource-group $RESOURCE_GROUP
+
+IDENTITY_ID=$(az identity show --name $IDENTITY --resource-group $RESOURCE_GROUP --query id --output tsv)
+
+docker build --tag $ACR_NAME.azurecr.io/$API_NAME ./src
+
+az group delete --name $RESOURCE_GROUP
